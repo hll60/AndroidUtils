b726912878486341484df56b1091be3a965f1535
huangliulin <1097191487@qq.com>
2016-07-14 12:45:20 +0800

    将showToast方法改为static方法

--- utils/src/main/java/com/hll/android/utils/HUtils.java
+++ utils/src/main/java/com/hll/android/utils/HUtils.java
@@ -61,11 +61,11 @@ public class HUtils {
         }
     }
 
-    public void showToast(String text){
+    public static void showToast(String text){
         Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
     }
 
-    public void showToastLong(String text){
+    public static void showToastLong(String text){
         Toast.makeText(mContext, text, Toast.LENGTH_LONG).show();
     }
 
